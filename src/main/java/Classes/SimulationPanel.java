7f668b0e2222da616709ee49b6020dd5ed61452d
Harm Haandrikman <hfhaandrikman@gmail.com>
2025-09-10 16:57:02 +0200

    update WIP. background

--- src/main/java/Classes/SimulationPanel.java
+++ src/main/java/Classes/SimulationPanel.java
@@ -1,13 +1,25 @@
 package Classes;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class SimulationPanel extends JPanel {
+
+    InputStream background = getClass().getResourceAsStream("/images/background.png");
+    final BufferedImage image = ImageIO.read(background);
+
+    public SimulationPanel() throws IOException {
+    }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.black);
+        g.drawImage(image, 0, 0, this);
         g.fillRect(100,100,50,50);
     }
 }
--- src/main/java/Classes/SupermarketSimulator.java
+++ src/main/java/Classes/SupermarketSimulator.java
@@ -1,16 +1,17 @@
 package Classes;
 
 import javax.swing.*;
+import java.io.IOException;
 
 public class SupermarketSimulator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         JFrame frame = new JFrame("Supermarket Simulator");
         JButton button = new JButton("Simulate");
         frame.add(button);
 
         frame.add(new SimulationPanel());
 
-        frame.setSize(400,400);
+        frame.setSize(1280,720);
         frame.setVisible(true);
     }
 }
