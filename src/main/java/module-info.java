5cf66aac99aa5049ed8784eab34b00b162738885
Harm Haandrikman <hfhaandrikman@gmail.com>
2025-09-10 16:38:43 +0200

    images en verwijderen onnodige files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,4 @@
 module com.example.supermarketsim {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires com.almasb.fxgl.all;
     requires java.desktop;
 
     opens com.example.supermarketsim to javafx.fxml;
