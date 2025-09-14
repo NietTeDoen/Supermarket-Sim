b1698fbfef52e5b647db1507273965b1ff088413
Harm Haandrikman <hfhaandrikman@gmail.com>
2025-09-14 19:05:28 +0200

    Venster is nu fullscreen en image resized

--- src/main/java/Classes/Main.java
+++ src/main/java/Classes/Main.java
@@ -3,8 +3,18 @@ package Classes;
 import javax.swing.*;
 import java.io.IOException;
 
-public class SupermarketSimulator {
+public class Main {
     public static void main(String[] args) throws IOException {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                CreateAndShowGUI();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private static void CreateAndShowGUI() throws IOException {
         JFrame frame = new JFrame("Supermarket Simulator");
         JButton button = new JButton("Simulate");
         frame.add(button);
@@ -12,6 +22,7 @@ public class SupermarketSimulator {
         frame.add(new SimulationPanel());
 
         frame.setSize(1280,720);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
     }
 }
--- src/main/java/Classes/SimulationPanel.java
+++ src/main/java/Classes/SimulationPanel.java
@@ -11,15 +11,15 @@ import java.io.InputStream;
 public class SimulationPanel extends JPanel {
 
     InputStream background = getClass().getResourceAsStream("/images/background.png");
-    final BufferedImage image = ImageIO.read(background);
+    BufferedImage image = ImageIO.read(background);
 
     public SimulationPanel() throws IOException {
     }
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.black);
-        g.drawImage(image, 0, 0, this);
-        g.fillRect(100,100,50,50);
+        int width = getWidth();
+        int height = getHeight();
+        g.drawImage(image, 0, 0, width, height, this);
     }
 }
