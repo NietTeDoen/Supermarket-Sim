d535b94b30ccc80202f99feeb30ebe6af73e902f
Harm Haandrikman <hfhaandrikman@gmail.com>
2025-09-15 18:35:19 +0200

    broken applicatie gefixed door child models afmaken

--- src/main/java/Model/Klant.java
+++ src/main/java/Model/Klant.java
@@ -1,4 +1,13 @@
 package Model;
 
+import java.util.List;
+import java.util.UUID;
+
 public class Klant extends  Person {
+    private List<Product> ProductsList;
+
+    public Klant(Float speed, Integer[] positie, Integer[] target, String sprite, List<Product> ProductsList) {
+        super(speed, positie, target, sprite);
+        this.ProductsList = ProductsList;
+    }
 }
--- src/main/java/Model/Medewerker.java
+++ src/main/java/Model/Medewerker.java
@@ -1,4 +1,12 @@
 package Model;
 
+import javax.swing.text.StyledEditorKit;
+
 public class Medewerker extends Person {
+    private String Taak;
+
+    public Medewerker(Float speed, Integer[] positie, Integer[] target, String sprite, String Taak) {
+        super(speed, positie, target, sprite);
+        this.Taak = Taak;
+    }
 }
