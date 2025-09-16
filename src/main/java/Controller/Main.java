259a903559b62bedbc1795af1337d1fba652341a
Harm Haandrikman <hfhaandrikman@gmail.com>
2025-09-16 14:25:23 +0200

    laatste model afgemaakt. moeten in sommige nog wel functies

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -1,5 +1,7 @@
 package Controller;
 
+import Model.Person;
+import Model.Winkelwagen;
 import View.SimulationPanel;
 
 import javax.swing.*;
--- src/main/java/Model/Winkelwagen.java
+++ src/main/java/Model/Winkelwagen.java
@@ -1,4 +1,19 @@
 package Model;
 
+import java.util.List;
+import java.util.UUID;
+
 public class Winkelwagen {
+    private UUID PersonID;
+    private List<Product> ProductList;
+
+    public Winkelwagen(UUID PersonID, List<Product> ProductList) {
+        this.PersonID = PersonID;
+        this.ProductList = ProductList;
+    }
+
+    public List<Product> AddProduct(Product Product) {
+        ProductList.add(Product);
+        return ProductList;
+    }
 }
