00a12fba143780e8eb84e163d155eb8ce04f0749
Harm Haandrikman <hfhaandrikman@gmail.com>
2025-09-24 15:22:02 +0200

    world class voor inspawnen objects

---
+++ src/main/java/Model/Logic/World.java
@@ -0,0 +1,27 @@
+package Model.Logic;
+
+import Model.People.Person;
+import Model.Store.Kassa;
+import Model.Store.Schap;
+import Model.Store.Winkelwagen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class World
+{
+    private List<Kassa> kassas = new ArrayList<Kassa>();
+    private List<Schap> Schappen =  new ArrayList<Schap>();
+    private List<Person> Persons = new ArrayList<Person>();
+
+    public  World()
+    {
+        kassas.add(new Kassa(null, null));
+
+        Schappen.add(new Schap(0,0));
+
+
+    }
+
+
+}
